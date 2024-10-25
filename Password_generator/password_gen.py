9207fc79f5f1fece65c4dab3f16f4c3078edb15f
JacobSmxth <jacob.d.smith@live.com>
2024-10-24 23:05:22 -0400

    Added all the fancy input stuff to the program. It looks very basic, but not as basic as lines on the terminal

--- Password_generator/password_gen.py
+++ Password_generator/password_gen.py
@@ -8,4 +8,37 @@ m = Tk()
 m.title("Password Generator")
 
 
+# function to generate password
+# I want to have a selection of choices to make the password that is generated, how the user wants
+def generate_password():
+    # Code in here
+    print("Password Generate")
+
+
+# Label for length slider
+length_label = Label(m, text="Length:")
+length_label.pack()
+
+# Slider or as tkinter calls it scale widget
+length_slider = Scale(m, from_=8, to=32, orient=HORIZONTAL, length=200, tickinterval=24)
+length_slider.set(16)
+length_slider.pack()
+
+# Variables to hold if checkbox is checked or not
+# Will add radio buttuns eventually to toggle between Passphrase, and Password
+lcletters_var = BooleanVar()
+ucletters_var = BooleanVar()
+numbers_var = BooleanVar()
+symbols_var = BooleanVar()
+
+# Checkboxes
+Checkbutton(m, text="a-z", variable=lcletters_var).pack()
+Checkbutton(m, text="A-Z", variable=ucletters_var).pack()
+Checkbutton(m, text="0-9", variable=numbers_var).pack()
+Checkbutton(m, text="!@#$%^&*", variable=symbols_var).pack()
+
+# Generate button
+generate_btn = Button(m, text=f"Generate Password", command=generate_password)
+generate_btn.pack()
+
 m.mainloop()
