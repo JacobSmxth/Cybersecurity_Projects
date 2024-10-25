02acf9133363bb19f4f1cd94941b0798071286e1
JacobSmxth <jacob.d.smith@live.com>
2024-10-25 00:04:02 -0400

    Many changes, noting process in README.md

--- Password_generator/password_gen.py
+++ Password_generator/password_gen.py
@@ -1,11 +1,42 @@
 import random
 import string
 from tkinter import *
+from tkinter import ttk
+import time
+import pyperclip
 
 # Create the main window for the generator
 # Tkinter is completely new to me so i'm learning this as I go and read the documentation
 m = Tk()
 m.title("Password Generator")
+
+
+# Calculating center as well
+screen_width = m.winfo_screenwidth()
+screen_height = m.winfo_screenheight()
+# Calculate x, y center coords
+x_axis = (screen_width // 2) - (800 // 2)
+y_axis = (screen_height // 2) - (600 // 2)
+
+# Resize the window to be larger and postion it
+m.geometry(f"800x600+{x_axis}+{y_axis}")
+
+
+# Copy password straight to clipboard
+def copyPassword(password):
+    pyperclip.copy(password)
+
+
+def is_valid_pass(password):
+    if lcletters_var.get() and not any(c in string.ascii_lowercase for c in password):
+        return False
+    if ucletters_var.get() and not any(c in string.ascii_uppercase for c in password):
+        return False
+    if numbers_var.get() and not any(c in string.digits for c in password):
+        return False
+    if symbols_var.get() and not any(c in "!@#$%^&*" for c in password):
+        return False
+    return True
 
 
 # function to generate password
@@ -29,12 +60,42 @@ def generate_password():
     # all available characters depending on inputs
     characters = ""
 
+    # If statements to gather which characters the user wants
     if lcletters_var.get():
         characters += string.ascii_lowercase
     if ucletters_var.get():
         characters += string.ascii_uppercase
     if numbers_var.get():
-        
+        characters += string.digits
+    if symbols_var.get():
+        characters += "!@#$%^&*"
+
+    # If statement to check that atleast one value was checked
+    if not characters:
+        print("Please select atleast one character type")
+        return
+
+    # Adding progress bar because I just found it
+    progress_bar = ttk.Progressbar(m, orient=HORIZONTAL, length=200, mode="determinate")
+    progress_bar.pack(pady=10)
+    progress_bar["value"] = 0
+    for i in range(101):
+        progress_bar["value"] = i
+        m.update_idletasks()
+        time.sleep(0.005)
+        if i == 100:
+            progress_bar.destroy()
+
+    # Generate password AND validate it!
+    while True:
+        password = "".join(random.choice(characters) for eachChar in range(length))
+        if is_valid_pass(password):
+            break
+
+    print(f"Generated Password: {password}")
+
+    copy_btn = Button(m, text="Copy Password", command=lambda: copyPassword(password))
+    copy_btn.pack()
 
 
 # Label for length slider
@@ -58,6 +119,7 @@ Checkbutton(m, text="a-z", variable=lcletters_var).pack()
 Checkbutton(m, text="A-Z", variable=ucletters_var).pack()
 Checkbutton(m, text="0-9", variable=numbers_var).pack()
 Checkbutton(m, text="!@#$%^&*", variable=symbols_var).pack()
+
 
 # Generate button
 generate_btn = Button(m, text=f"Generate Password", command=generate_password)
