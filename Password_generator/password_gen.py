9a44c754d196b6a1aec015d41b4642a76c4f20d0
JacobSmxth <jacob.d.smith@live.com>
2024-10-24 22:44:24 -0400

    Added some lines of code. Currently learning the tkinter library

--- Password_generator/password_gen.py
+++ Password_generator/password_gen.py
@@ -1,2 +1,11 @@
 import random
 import string
+from tkinter import *
+
+# Create the main window for the generator
+# Tkinter is completely new to me so i'm learning this as I go and read the documentation
+m = Tk()
+m.title("Password Generator")
+
+
+m.mainloop()
